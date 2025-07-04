642d9f0e9e4d9d04fc304ddb5d893026d275dc29
Alpha <124107448+Gomer123rb@users.noreply.github.com>
2025-07-04 09:31:29 -0400

    Update PXIVec3.h
    gonna fix that .h file too

--- NTint/intelPXI/he_2/include/PXIVec3.h
+++ NTint/intelPXI/he_2/include/PXIVec3.h
@@ -3,13 +3,24 @@
 struct PXIVec3 {
     float x, y, z;
 
+    float xlighting, ylighting, zlighting
+
+    float xl, yl, zl
+
     PXIVec3() : x(0), y(0), z(0) {}
     PXIVec3(float x, float y, float z) : x(x), y(y), z(z) {}
+    PXIVec3(float xlighting, float ylighting, float zlighting) : xlighting(xl), ylighting(yl), zlighting(zl)
 
     PXIVec3 operator+(const PXIVec3& other) const { return PXIVec3(x + other.x, y + other.y, z + other.z); }
     PXIVec3 operator-(const PXIVec3& other) const { return PXIVec3(x - other.x, y - other.y, z - other.z); }
     PXIVec3 operator*(float scalar) const { return PXIVec3(x * scalar, y * scalar, z * scalar); }
+    PXIVec3 operator+(const PXIVec3& other) const { return PXIVec3(x + other.xlighting, y + other.ylighting, z + other.zlighting); }
+    PXIVec3 operator-(const PXIVec3& other) const { return PXIVec3(x - other.xlighting, y - other.ylighting, z - other.zlighting); }
+    PXIVec3 operator*(float scalar) const { return PXIVec3(xlighting * scalar, ylighting * scalar, zlighting * scalar); }
 
     PXIVec3 normalized() const;
     float length() const;
 };
+//damn it i named it after the physX api
+// who cares tho
+// at least NVIDIA can use this script, I don't mind.
