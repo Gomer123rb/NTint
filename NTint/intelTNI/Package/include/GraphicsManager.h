ba443176687220795a0c33f1f88f7be059b8b8b7
Alpha <124107448+Gomer123rb@users.noreply.github.com>
2025-07-04 14:15:44 -0400

    Update GraphicsManager.h
    rtx

--- NTint/intelTNI/Package/include/GraphicsManager.h
+++ NTint/intelTNI/Package/include/GraphicsManager.h
@@ -2,15 +2,19 @@
 #define GRAPHICSMANAGER_H
 
 #include <d3d11.h>
+#include <DirectXMath.h>  // For XMFLOAT4
 
 namespace DirectMod {
     class GraphicsManager {
     public:
         static bool Initialize(HWND hwnd);
-        static void Clear(float r, float g, float b, float a);
+        static void Clear(float r, float g, float b, float a, bool useNTINTAmbient = false);
         static void Present();
         static void Shutdown();
+
     private:
+        static XMFLOAT4 GetSimulatedRayClearColor();
+
         static ID3D11Device* device;
         static ID3D11DeviceContext* context;
         static IDXGISwapChain* swapChain;
